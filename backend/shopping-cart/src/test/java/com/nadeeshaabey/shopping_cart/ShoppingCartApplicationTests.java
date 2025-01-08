26efecefa4f95e679ae3e13678431faefdad866c
Nadeesha Abeywickrama <127606065+NadeeshaAbey@users.noreply.github.com>
2025-01-08 12:56:20 +0530

    Initial commit

---
+++ backend/shopping-cart/src/test/java/com/nadeeshaabey/shopping_cart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadeeshaabey.shopping_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
