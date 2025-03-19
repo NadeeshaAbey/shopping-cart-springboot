825aa5d0ec69b14fcc1762eadebd5bc0174f2ea9
Nadeesha Abeywickrama <127606065+NadeeshaAbey@users.noreply.github.com>
2025-03-19 10:15:27 +0530

    implement order and order list models

---
+++ backend/shopping-cart/src/main/java/com/nadeeshaabey/shopping_cart/model/Order.java
@@ -0,0 +1,32 @@
+package com.nadeeshaabey.shopping_cart.model;
+
+import com.nadeeshaabey.shopping_cart.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long oderId;
+    private LocalDate oderDate;
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems;
+}
