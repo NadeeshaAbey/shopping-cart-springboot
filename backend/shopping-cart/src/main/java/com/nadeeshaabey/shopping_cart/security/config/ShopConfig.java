16ca2a71a0fe49933bcdc55dcf0e8c9ce9d38239
Nadeesha Abeywickrama <127606065+NadeeshaAbey@users.noreply.github.com>
2025-04-15 12:49:22 +0530

    create jwtutils class

--- backend/shopping-cart/src/main/java/com/nadeeshaabey/shopping_cart/security/config/ShopConfig.java
+++ backend/shopping-cart/src/main/java/com/nadeeshaabey/shopping_cart/security/config/ShopConfig.java
@@ -1,4 +1,4 @@
-package com.nadeeshaabey.shopping_cart.config;
+package com.nadeeshaabey.shopping_cart.security.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
